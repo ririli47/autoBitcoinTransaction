4cf73f9da2d3410ad4a055618819adf338b3c33c
Naoya Fukuhara <suguha47@gmail.com>
2019-01-18 08:37:56 +0900

    一歩前進

--- exponentialMA.js
+++ exponentialMA.js
@@ -20,87 +20,77 @@ const sleep = timer => {
   });
 };
 
-/* 1日目の平均値を計算 */
-//Dateオブジェクト生成
-let date = new Date();
-console.log(Math.floor(date.getTime() / 1000));
-//42分前にセット
-date.setMinutes(date.getMinutes() - 42);
-let after = Math.floor(date.getTime() / 1000);
-console.log(after);
+function CulcFirstDay() {
+  /* 1分目の平均値を計算 */
+  //Dateオブジェクト生成
+  let date = new Date();
+//   console.log(Math.floor(date.getTime() / 1000));
+  //仮に30分前にセット
+  date.setMinutes(date.getMinutes() - 30);
+//   let after = Math.floor(date.getTime() / 1000);
+  console.log(after);
 
-// APIアクセスafterから60秒ごとのデータを取得
-const options = {
-  url: "https://api.cryptowat.ch/markets/bitflyer/btcjpy/ohlc",
-  method: "GET",
-  qs: {
-    periods: 60,
-    after: after
-  }
-};
+  // APIアクセス afterから60秒ごとのデータを取得
+  const options = {
+    url: "https://api.cryptowat.ch/markets/bitflyer/btcjpy/ohlc",
+    method: "GET",
+    qs: {
+      periods: 60,
+      after: after
+    }
+  };
 
-request(options)
-  .then(function(body) {
-    console.log("-----------------------------------------");
-    let result = JSON.parse(body);
-  })
-  .catch(function(err) {
-    console.log(err);
-  })(async function() {
+  request(options)
+    .then(function(body) {
+      console.log("-----------------------------------------");
+      let result = JSON.parse(body);
+    //   console.log(result.result["60"]);
+      //21分前から現在までの単純平均を計算
+      let average = 0;
+      for (
+        let i = result.result["60"].length - 1;
+        i >= result.result["60"].length - 21;
+        i--
+      ) {
+        average += result.result["60"][i][4];
+        console.log(
+          i +
+            " : " +
+            result.result["60"][i][0] +
+            " : " +
+            result.result["60"][i][4]
+        );
+      }
+      average = average / 21
+      return average;
+    })
+    .catch(function(err) {
+      console.log(err)
+    });
+}
+
+(async function() {
+  let firstTermFlg = true;
+  let yesterdayAverage = 0
   while (true) {
-    // console.log(result.result['60'])
-
-    for (let i = result.result["60"].length - 1; i >= 0; i--) {
-      console.log(
-        i +
-          " : " +
-          result.result["60"][i][0] +
-          " : " +
-          result.result["60"][i][4]
-      );
-    }
-
-    //指数平滑移動平均計算開始
-    let exp = Array();
-    let exp_1 = 0;
-
-    //1分目は完全に平均
-    //0~20までで初日の平均を出す
-    for (let i = 0; i < 21; i++) {
-      exp_1 += result.result["60"][i][4];
-    }
-    exp.push(exp_1 / 21);
-    console.log("day 1 : " + exp[0]);
-
-    //2分目からは計算
-    //21~42で計算する
-    for (let i = 21; i < 42; i++) {
-      exp[i - 20] =
-        exp[i - 21] +
-        (2 / (21 + 1)) * (result.result["60"][i][4] - exp[i - 21]);
-      console.log("day " + (i - 19) + " : " + exp[i - 20]);
+    if (firstTermFlg) {
+            yesterdayAverage =  CulcFirstDay() //requestが非同期で動いてる
+            console.log(yesterdayAverage)
+            firstTermFlg = false
     }
 
     //現在の値
     (async function() {
-      const config = require("./config");
-      const bitflyer = new ccxt.bitflyer(config);
-      const ticker = await bitflyer.fetchTicker("FX_BTC_JPY");
-      console.log("ask : " + ticker.ask);
-      exp[21] = exp[20] + (2 / (21 + 1)) * (ticker.ask - exp[20]);
+      const config = require("./config")
+      const bitflyer = new ccxt.bitflyer(config)
+      const ticker = await bitflyer.fetchTicker("FX_BTC_JPY")
+      console.log("ask : " + ticker.ask)
+      //   exp[21] = exp[20] + (2 / (21 + 1)) * (ticker.ask - exp[20]);
+      const todayAverage = yesterdayAverage + (2 / (21 + 1)) * (ticker.ask - yesterdayAverage)
+      console.log('todayAverage : ' + todayAverage)
+      yesterdayAverage = todayAverage
     })();
 
-    await sleep(interval);
+    await sleep(interval)
   }
 })();
-
-// (async function () {
-//     const bitflyer = new ccxt.bitflyer (config)
-
-//     while (true) {
-//         const ticker = await bitflyer.fetchTicker ('FX_BTC_JPY')
-//         records.push(ticker.ask)
-
-//         await sleep(interval)
-//     }
-// }) ();
--- main.js
+++ main.js
@@ -1,142 +1,148 @@
-'use strict';
-const ccxt = require ('ccxt')
-const config = require ('./config')
-const env = require ('./env')
+"use strict";
+const ccxt = require("ccxt");
+const config = require("./config");
+const env = require("./env");
 
-const interval = 10000
-const profitPrice = 500
-const lossCutPrice = -500
-const orderSize = 0.01
-const records = []
+const interval = 10000;
+const profitPrice = 500;
+const lossCutPrice = -500;
+const orderSize = 0.01;
+const records = [];
 
-let orderInfo = null
-let allSales = 0
+let orderInfo = null;
+let allSales = 0;
 
+const sleep = timer => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, timer);
+  });
+};
 
-const sleep = (timer) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve()
-        }, timer)
-    })
-}
+(async function() {
+  const bitflyer = new ccxt.bitflyer(config);
 
-(async function () {
-    const bitflyer = new ccxt.bitflyer (config)
-
-    while (true) {
-        const ticker = await bitflyer.fetchTicker ('FX_BTC_JPY')
-        records.push(ticker.ask)
-        if(records.length > 4) {
-            records.shift()
-        }
-        console.log(records)
-        if(orderInfo) {
-            console.log('latest bid price:', ticker.bid)
-            console.log('order price     :', orderInfo.price)
-            console.log('diff            :', ticker.bid - orderInfo.price)
-            if(orderInfo.position == 'buy') {
-                if(ticker.bid - orderInfo.price > profitPrice) {
-                    let order = null
-                    if(env.production) {
-                        order = await bitflyer.createMarketSellOrder ('FX_BTC_JPY', orderSize)
-                    }
-                    else {
-                        order = 'fuga'
-                    }
-
-                    allSales += (ticker.bid - orderInfo.price)
-                    console.log('利確しました' + "\n", order)
-                    console.log('単収支報告：', ticker.ask - orderInfo.price)
-                    console.log('総収支報告：', allSales)
-
-                    orderInfo = null
-                } else if (ticker.bid - orderInfo.price < lossCutPrice) {
-                    let order = null
-                    if(env.production) {
-                        order = await bitflyer.createMarketSellOrder ('FX_BTC_JPY', orderSize)
-                    }
-                    else {
-                        order = 'fuga'
-                    }
-                    allSales += (ticker.bid - orderInfo.price)
-                    console.log('ロスカットしました' + "\n", order)
-                    console.log('単収支報告：', ticker.ask - orderInfo.price)
-                    console.log('総収支報告：', allSales)
-
-                    orderInfo = null
-                }
-            }
-            else if (orderInfo.position = 'sell') {
-                if(orderInfo.price - ticker.bid > profitPrice) {
-                    let order = null
-                    if(env.production) {
-                        order = await bitflyer.createMarketBuyOrder ('FX_BTC_JPY', orderSize)
-                    }
-                    else {
-                        order = 'fuga'
-                    }
-
-                    allSales += (ticker.bid - orderInfo.price)
-                    console.log('利確しました' + "\n", order)
-                    console.log('単収支報告：', ticker.ask - orderInfo.price)
-                    console.log('総収支報告：', allSales)
-
-                    orderInfo = null
-                } else if (orderInfo.price - ticker.bid < lossCutPrice) {
-                    let order = null
-                    if(env.production) {
-                        order = await bitflyer.createMarketBuyOrder ('FX_BTC_JPY', orderSize)
-                    }
-                    else {
-                        order = 'fuga'
-                    }
-                    allSales += (ticker.bid - orderInfo.price)
-                    console.log('ロスカットしました' + "\n", order)
-                    console.log('単収支報告：', ticker.ask - orderInfo.price)
-                    console.log('総収支報告：', allSales)
-
-                    orderInfo = null
-                }
-            }
-        }
-        else {
-            if( records[0] < records[1] &&
-                records[1] < records[2] && 
-                records[2] < records[3] ) {
-                let order = null
-                if(env.production) {
-                    order = await bitflyer.createMarketBuyOrder ('FX_BTC_JPY', orderSize)
-                }
-                else {
-                    order = 'hoge'
-                }
-                orderInfo = {
-                    order: order,
-                    price: ticker.ask,
-                    position: 'buy'
-                }
-                console.log('買い注文を実施しました' + "\n", orderInfo)
-                // sendToSlack('買い注文を実施しました')
-            }
-            else if(records[3] < records[2] &&
-                    records[2] < records[1] && 
-                    records[1] < records[0] ) {
-                let order = null
-                if(env.production) {
-                    order = await bitflyer.createMarketSellOrder ('FX_BTC_JPY', orderSize)
-                }
-                else {
-                    order = 'hoge'
-                }
-                orderInfo = {
-                    order: order,
-                    price: ticker.ask,
-                    position: 'sell'
-                }
-                console.log('売り注文を実施しました' + "\n", orderInfo)        
-            }
-        }
-        await sleep(interval)
+  while (true) {
+    const ticker = await bitflyer.fetchTicker("FX_BTC_JPY");
+    records.push(ticker.ask);
+    if (records.length > 4) {
+      records.shift();
     }
-}) ();
+    console.log(records);
+    if (orderInfo) {
+      console.log("latest bid price:", ticker.bid);
+      console.log("order price     :", orderInfo.price);
+      console.log("diff            :", ticker.bid - orderInfo.price);
+      if (orderInfo.position == "buy") {
+        if (ticker.bid - orderInfo.price > profitPrice) {
+          let order = null;
+          if (env.production) {
+            order = await bitflyer.createMarketSellOrder(
+              "FX_BTC_JPY",
+              orderSize
+            );
+          } else {
+            order = "fuga";
+          }
+
+          allSales += ticker.bid - orderInfo.price;
+          console.log("利確しました" + "\n", order);
+          console.log("単収支報告：", ticker.ask - orderInfo.price);
+          console.log("総収支報告：", allSales);
+
+          orderInfo = null;
+        } else if (ticker.bid - orderInfo.price < lossCutPrice) {
+          let order = null;
+          if (env.production) {
+            order = await bitflyer.createMarketSellOrder(
+              "FX_BTC_JPY",
+              orderSize
+            );
+          } else {
+            order = "fuga";
+          }
+          allSales += ticker.bid - orderInfo.price;
+          console.log("ロスカットしました" + "\n", order);
+          console.log("単収支報告：", ticker.ask - orderInfo.price);
+          console.log("総収支報告：", allSales);
+
+          orderInfo = null;
+        }
+      } else if ((orderInfo.position = "sell")) {
+        if (orderInfo.price - ticker.bid > profitPrice) {
+          let order = null;
+          if (env.production) {
+            order = await bitflyer.createMarketBuyOrder(
+              "FX_BTC_JPY",
+              orderSize
+            );
+          } else {
+            order = "fuga";
+          }
+
+          allSales += ticker.bid - orderInfo.price;
+          console.log("利確しました" + "\n", order);
+          console.log("単収支報告：", ticker.ask - orderInfo.price);
+          console.log("総収支報告：", allSales);
+
+          orderInfo = null;
+        } else if (orderInfo.price - ticker.bid < lossCutPrice) {
+          let order = null;
+          if (env.production) {
+            order = await bitflyer.createMarketBuyOrder(
+              "FX_BTC_JPY",
+              orderSize
+            );
+          } else {
+            order = "fuga";
+          }
+          allSales += ticker.bid - orderInfo.price;
+          console.log("ロスカットしました" + "\n", order);
+          console.log("単収支報告：", ticker.ask - orderInfo.price);
+          console.log("総収支報告：", allSales);
+
+          orderInfo = null;
+        }
+      }
+    } else {
+      if (
+        records[0] < records[1] &&
+        records[1] < records[2] &&
+        records[2] < records[3]
+      ) {
+        let order = null;
+        if (env.production) {
+          order = await bitflyer.createMarketBuyOrder("FX_BTC_JPY", orderSize);
+        } else {
+          order = "hoge";
+        }
+        orderInfo = {
+          order: order,
+          price: ticker.ask,
+          position: "buy"
+        };
+        console.log("買い注文を実施しました" + "\n", orderInfo);
+        // sendToSlack('買い注文を実施しました')
+      } else if (
+        records[3] < records[2] &&
+        records[2] < records[1] &&
+        records[1] < records[0]
+      ) {
+        let order = null;
+        if (env.production) {
+          order = await bitflyer.createMarketSellOrder("FX_BTC_JPY", orderSize);
+        } else {
+          order = "hoge";
+        }
+        orderInfo = {
+          order: order,
+          price: ticker.ask,
+          position: "sell"
+        };
+        console.log("売り注文を実施しました" + "\n", orderInfo);
+      }
+    }
+    await sleep(interval);
+  }
+})();

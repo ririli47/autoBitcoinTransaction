da0b72b965fc750a1540333afc5ec15498214910
Naoya Fukuhara <suguha47@gmail.com>
2019-01-31 00:29:26 +0900

    test.jsをアップロード

--- test.js
+++ test.js
@@ -25,78 +25,10 @@ const sleep = (timer) => {
 
     while (true) {
         const ticker = await bitflyer.fetchTicker ('FX_BTC_JPY')
-        records.push(ticker.ask)
-        if(records.length > 3) {
-            records.shift()
-            console.log(records)
 
-            if(orderInfo) {
-                if(orderInfo.position == 'buy') {
-                    if(records[0] > records[1]) {
-                        let order = null
-                        if(env.production) {
-                            order = await bitflyer.createMarketSellOrder ('FX_BTC_JPY', orderSize)
-                        }
-                        else {
-                            order = 'fuga'
-                        }
-                        allSales +=  (ticker.ask - orderInfo.price)
-                        console.log('売り注文を実施しました' + "\n", order)
-                        console.log('単収支報告：', ticker.ask - orderInfo.price)
-                        console.log('総収支報告：', allSales)
-                        orderInfo = null
-                    }
-                }
-                else if(orderInfo.position == 'sell') {
-                    if(records[1] > records[0]) {
-                        let order = null
-                        if(env.production) {
-                            order = await bitflyer.createMarketBuyOrder ('FX_BTC_JPY', orderSize)
-                        }
-                        else {
-                            order = 'fuga'
-                        }
-                        allSales +=  (orderInfo.price - ticker.ask)
-                        console.log('買い注文を実施しました' + "\n", order)
-                        console.log('単収支報告：', orderInfo.price - ticker.ask)
-                        console.log('総収支報告：', allSales)
-                        orderInfo = null
-                    }    
-                }
-            }
-            else {
-                if(records[0] < records[1] && records[1] < records[2]) {
-                    let order = null
-                    if(env.production) {
-                        order = await bitflyer.createMarketBuyOrder ('FX_BTC_JPY', orderSize)
-                    }
-                    else {
-                        order = 'hoge'
-                    }
-                    orderInfo = {
-                        order: order,
-                        price: ticker.ask,
-                        position: 'buy'
-                    }
-                    console.log('買い注文を実施しました' + "\n", orderInfo)
-                }
-                else if (records[2] < records[1] && records[1] < records[0]) {
-                    let order = null
-                    if(env.production) {
-                        order = await bitflyer.createMarketSellOrder ('FX_BTC_JPY', orderSize)
-                    }
-                    else {
-                        order = 'hoge'
-                    }
-                    orderInfo = {
-                        order: order,
-                        price: ticker.ask,
-                        position: 'sell'
-                    }
-                    console.log('売り注文を実施しました' + "\n", orderInfo)
-                }
-            }
-        }
+        let result = bitfler.privateGetGetchildorders();
+
+        console.log(result)
         await sleep(interval)
     }
 }) ();
